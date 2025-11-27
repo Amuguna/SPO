150abbe594e16d3838b433bcea06238c8531e296
Amuguna <junjang99@naver.com>
2025-11-27 09:32:01 +0000

    [add] llama configs

---
+++ run_llama.sh
@@ -0,0 +1,6 @@
+export MASTER_PORT=$(python -c "import socket; s=socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
+export APP_SEED="42"
+export NUM_GPUS=8
+# WANDB_PROJECT=spo-math APP_EXPERIMENT_NAME=qwen2.5_7b-spo-tree-666 APP_DIRECTORY=SPO-experiments APP_MINIMIZE_STORED_FILES=True deepspeed --no_local_rank --num_gpus=$NUM_GPUS --master_port $MASTER_PORT --include localhost:0  src/treetune/main.py --configs "configs/qwen_2_5_7b_math_spo_chain_MATH.jsonnet" run_iteration_loop
+
+WANDB_PROJECT=spo-math APP_EXPERIMENT_NAME=llama3_1_8b-spo-tree-666 APP_DIRECTORY=SPO-experiments APP_MINIMIZE_STORED_FILES=True deepspeed --no_local_rank --num_gpus=$NUM_GPUS --master_port $MASTER_PORT src/treetune/main.py --configs "configs/llama3_1_8b_spo_chain_MATH.jsonnet" run_iteration_loop
